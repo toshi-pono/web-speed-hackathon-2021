f645876ff7bffc64f806e9127fbaf47d1653ea1d
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2021-12-28 17:06:12 +0900

    EnvironmentPlugin?わからんけどとりあえずproductionに

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -65,7 +65,7 @@ const config = {
       BUILD_DATE: new Date().toISOString(),
       // Heroku では SOURCE_VERSION 環境変数から commit hash を参照できます
       COMMIT_HASH: process.env.SOURCE_VERSION || '',
-      NODE_ENV: 'development',
+      NODE_ENV: 'production',
     }),
     new MiniCssExtractPlugin({
       filename: 'styles/[name].css',
