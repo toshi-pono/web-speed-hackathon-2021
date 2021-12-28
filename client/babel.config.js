5cc569383f19e0e641f444053cd9426706b39281
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2021-12-28 15:19:47 +0900

    module auto

--- client/babel.config.js
+++ client/babel.config.js
@@ -4,7 +4,7 @@ module.exports = {
       '@babel/preset-env',
       {
         corejs: '3',
-        modules: 'commonjs',
+        modules: 'auto',
         targets: 'last 1 Chrome major version',
         useBuiltIns: 'usage',
       },
