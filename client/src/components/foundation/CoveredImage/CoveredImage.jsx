347d94f5878db8353c30079953c83f3c7e5b3fbe
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2021-12-29 12:06:41 +0900

    画像のトリミングをCSSで実行

--- client/src/components/foundation/CoveredImage/CoveredImage.jsx
+++ client/src/components/foundation/CoveredImage/CoveredImage.jsx
@@ -18,38 +18,19 @@ import { fetchBinary } from '../../../utils/fetchers';
 const CoveredImage = ({ alt, src }) => {
   const { data, isLoading } = useFetch(src, fetchBinary);
 
-  const imageSize = React.useMemo(() => {
-    return data !== null ? sizeOf(Buffer.from(data)) : null;
-  }, [data]);
-
   const blobUrl = React.useMemo(() => {
     return data !== null ? URL.createObjectURL(new Blob([data])) : null;
   }, [data]);
-
-  const [containerSize, setContainerSize] = React.useState({ height: 0, width: 0 });
-  /** @type {React.RefCallback<HTMLDivElement>} */
-  const callbackRef = React.useCallback((el) => {
-    setContainerSize({
-      height: el?.clientHeight ?? 0,
-      width: el?.clientWidth ?? 0,
-    });
-  }, []);
 
   if (isLoading || data === null || blobUrl === null) {
     return null;
   }
 
-  const containerRatio = containerSize.height / containerSize.width;
-  const imageRatio = imageSize?.height / imageSize?.width;
-
   return (
-    <div ref={callbackRef} className="relative w-full h-full overflow-hidden">
+    <div className="w-full h-full overflow-hidden">
       <img
         alt={alt}
-        className={classNames('absolute left-1/2 top-1/2 max-w-none transform -translate-x-1/2 -translate-y-1/2', {
-          'w-auto h-full': containerRatio > imageRatio,
-          'w-full h-auto': containerRatio <= imageRatio,
-        })}
+        className="object-cover w-full h-full"
         src={blobUrl}
       />
     </div>
