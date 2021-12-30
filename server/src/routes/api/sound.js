1f96d18c0a12d9758ad29e3eeac4acf4d5aea590
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2021-12-30 16:44:23 +0900

    metaFileの生成

--- server/src/routes/api/sound.js
+++ server/src/routes/api/sound.js
@@ -5,6 +5,7 @@ import Router from 'express-promise-router';
 import httpErrors from 'http-errors';
 import { v4 as uuidv4 } from 'uuid';
 
+import { calculate_wave } from '../../converters/generate_sound_wave';
 import { convertSound } from '../../converters/convert_sound';
 import { UPLOAD_PATH } from '../../paths';
 import { extractMetadataFromSound } from '../../utils/extract_metadata_from_sound';
@@ -13,6 +14,23 @@ import { extractMetadataFromSound } from '../../utils/extract_metadata_from_soun
 const EXTENSION = 'mp3';
 
 const router = Router();
+
+const convertFile = async (file, soundId) => {
+  const converted = await convertSound(file, {
+    // 音声の拡張子を指定する
+    extension: EXTENSION,
+  });
+
+  const filePath = path.resolve(UPLOAD_PATH, `./sounds/${soundId}.${EXTENSION}`);
+  await fs.writeFile(filePath, converted);
+};
+
+const generateMetaFile = async (file, soundId) => {
+  const { max, peaks } = await calculate_wave(file);
+  const metaFilePath = path.resolve(UPLOAD_PATH, `./sounds/${soundId}.${EXTENSION}.json`);
+  const metaFile = JSON.stringify({ max, peaks });
+  await fs.writeFile(metaFilePath, metaFile);
+};
 
 router.post('/sounds', async (req, res) => {
   if (req.session.userId === undefined) {
@@ -26,13 +44,7 @@ router.post('/sounds', async (req, res) => {
 
   const { artist, title } = await extractMetadataFromSound(req.body);
 
-  const converted = await convertSound(req.body, {
-    // 音声の拡張子を指定する
-    extension: EXTENSION,
-  });
-
-  const filePath = path.resolve(UPLOAD_PATH, `./sounds/${soundId}.${EXTENSION}`);
-  await fs.writeFile(filePath, converted);
+  await Promise.all([convertFile(req.body, soundId), generateMetaFile(req.body, soundId)]);
 
   return res.status(200).type('application/json').send({ artist, id: soundId, title });
 });
