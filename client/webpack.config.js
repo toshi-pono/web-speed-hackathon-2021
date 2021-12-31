a62480053ca3752c829edadc18d1e5d7958753cb
toshi-pono <66683209+toshi-pono@users.noreply.github.com>
2021-12-31 18:04:08 +0900

    standardized-audio-contextを削除

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -56,7 +56,6 @@ const config = {
   plugins: [
     new webpack.ProvidePlugin({
       $: 'jquery',
-      AudioContext: ['standardized-audio-context', 'AudioContext'],
       Buffer: ['buffer', 'Buffer'],
       'window.jQuery': 'jquery',
     }),
